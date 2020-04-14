770839f151ed5b4f85fe9d0bf171569643cf1338
Maryam <myusuf@afexnigeria.com>
2020-04-14 17:18:16 +0100

    updated the data input format

--- src/estimator.py
+++ src/estimator.py
@@ -15,15 +15,16 @@ def get_period_days(period_type, period_days):
 
 
 def impact_estimator(input_data):
-    num_of_days = get_period_days(input_data.get('periodType'), input_data.get('timeToElapse'))
+    num_of_days = get_period_days(input_data.get('data').get('periodType'), input_data.get('data').get('timeToElapse'))
     num_of_days = int(num_of_days / 3)
+    reportedCases = int(input_data.get('data').get('reportedCases'))
     output_data = {}
     impact = {}
     severe_impact = {}
 
-    currently_infected_impact = int(input_data.get('reportedCases') * 10)
+    currently_infected_impact = reportedCases * 10
     infections_by_requested_time_impact = int(currently_infected_impact * math.pow(2, num_of_days))
-    currently_infected_severe_impact = int(input_data.get('reportedCases') * 50)
+    currently_infected_severe_impact = reportedCases * 50
     infections_by_requested_time_severe_impact = int(currently_infected_severe_impact * math.pow(2, num_of_days))
 
     impact['currentlyInfected'] = currently_infected_impact
